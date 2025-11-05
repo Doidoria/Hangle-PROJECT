6501d64c39750fd9b5183442fa4cdc3959df2112
Doidoria <th15926@daum.net>
2025-11-05 22:57:34 +0900

    .

--- BN/src/main/java/com/example/demo/domain/competition/repository/CompetitionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.domain.competition.repository;
-
-import com.example.demo.domain.competition.entity.Competition;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompetitionRepository extends JpaRepository<Competition, Long>{
-
-}
