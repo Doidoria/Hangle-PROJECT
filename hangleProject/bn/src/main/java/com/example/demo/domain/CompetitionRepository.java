9ab4f38e36fbf426d9b2ccb49e5d110d70550546
seodongseong <ehdtjd0821@naver.com>
2025-11-10 16:17:04 +0900

    다 날리고 병합하기

--- hangleProject/bn/src/main/java/com/example/demo/domain/CompetitionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.domain;
-
-import com.example.demo.domain.entity.Competition;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompetitionRepository extends JpaRepository<Competition, Long> {
-}
