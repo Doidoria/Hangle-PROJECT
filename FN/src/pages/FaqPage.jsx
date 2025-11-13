af181d456ba20e4029056fb111f56c592229d7d0
jeonikk <meister1213@naver.com>
2025-11-13 17:43:39 +0900

    11.13 (고객센터 관련 페이지 완성)

--- FN/src/pages/FaqPage.jsx
+++ FN/src/pages/FaqPage.jsx
@@ -6,10 +6,10 @@ import "../css/FaqPage.scss";
 const faqData = [
     { id: 1, category: "회원", question: "회원가입은 어떻게 하나요?", answer: "로그인 페이지 하단의 '회원가입'을 통해 가입하실 수 있습니다." },
     { id: 2, category: "회원", question: "비밀번호를 잊어버렸어요.", answer: "로그인 페이지 하단의 '비밀번호 찾기'를 통해 재설정할 수 있습니다." },
-    { id: 3, category: "서비스", question: "작성한 문의는 어디서 확인하나요?", answer: "마이페이지 > 1:1 문의 내역에서 확인 가능합니다." },
+    { id: 3, category: "서비스", question: "작성한 문의는 어디서 확인하나요?", answer: "고객센터 페이지 하단의 '나의 문의 목록'을 통해 확인 가능합니다." },
     { id: 4, category: "서비스", question: "답변은 얼마나 걸리나요?", answer: "평균적으로 1~2 영업일 이내에 답변드립니다." },
     { id: 5, category: "기타", question: "고객센터 이용시간은 어떻게 되나요?", answer: "고객센터는 평일 오전 9시부터 오후 6시까지 운영됩니다." },
-    { id: 6, category: "기타", question: "점심메뉴 추천 해주세요", answer: "돈까스 ㄱㄱ" },
+    { id: 6, category: "기타", question: "점심메뉴 추천 해주세요", answer: "순대국밥·뼈해장국·콩나물국밥·김치찌개·된장찌개·부대찌개·김밥·라면·떡볶이·순대·튀김·어묵·돈까스·카레" },
     { id: 7, category: "기타", question: "수료날짜는 어떻게 되나요?", answer: "수료 날짜는 2025.12.05 입니다." },
 ];
 
