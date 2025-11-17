5542d5a30f060fa717f0b4b1944b0d6005cfd907
hye-yoen <tgeelhy@gmail.com>
2025-11-17 17:53:42 +0900

    정리

--- Leaderboard - basic/src/main/java/com/example/demo/domain/repository/SubmitRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.domain.repository;
-
-import com.example.demo.domain.entity.Submit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubmitRepository extends JpaRepository<Submit,Long> {
-
-}
