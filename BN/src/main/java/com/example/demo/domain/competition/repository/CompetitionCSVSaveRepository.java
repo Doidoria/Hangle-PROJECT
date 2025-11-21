75aa7de0076ef3b6c0a88cc6a048ca23fad442ce
Doidoria <th15926@daum.net>
2025-11-21 12:33:59 +0900

    .

--- BN/src/main/java/com/example/demo/domain/competition/repository/CompetitionCSVSaveRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.domain.competition.repository;
-
-import com.example.demo.domain.competition.entity.CompetitionCSVSave;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompetitionCSVSaveRepository extends JpaRepository<CompetitionCSVSave, Long> {
-}
