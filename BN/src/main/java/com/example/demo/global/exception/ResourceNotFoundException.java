69d6319e52fb7059cb2af66c4e39b7b54dc5867e
Doidoria <th15926@daum.net>
2025-11-24 13:30:29 +0900

    .

--- BN/src/main/java/com/example/demo/global/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.global.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
