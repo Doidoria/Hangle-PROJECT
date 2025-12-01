1c7f58fa76905f924a83b6df499cc040acc59b8a
Doidoria <th15926@daum.net>
2025-12-01 11:44:39 +0900

    .

--- FN/src/api/axiosConfig.js
+++ FN/src/api/axiosConfig.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
   withCredentials: true,
+  timeout: 600000,
 });
 
 // ------------------------
